372f7e5f8f037108b2d707e0786c0ed5df8a3162
nerdsquirrel <mrmanik.kuet@gmail.com>
2019-06-09 20:21:00 +0600

    refactoring

---
+++ Scripts/MailSending.ps1
@@ -0,0 +1,5 @@
+Send-MailMessage -SMTPServer localhost 
+-To mahbubur.rahman@gmail.com 
+-From mahbubur.rahman@gmail.com -
+Subject "This is a test email" 
+-Body "Hi, this is a test email using gmail as a smart host"
