8079f7ee482c02c569f815aecaaf3e75e38db92f
nerdsquirrel <mrmanik.kuet@gmail.com>
2020-12-28 19:08:32 +0600

    update script

--- PowerShell/FIndProcessUsingAnySpecificPort.ps1
+++ PowerShell/FIndProcessUsingAnySpecificPort.ps1
@@ -1,3 +1,7 @@
 $port_id = 8080
 netstat -aon | findstr $port_id
-tasklist /FI "PID eq process_id"
+# for example 3192 is the process id we found
+tasklist /FI "PID eq 3192"
+
+# kill the process with id
+taskkill /PID 3192 /F
